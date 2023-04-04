848c92b286648d1f13140cf31fd07fb71720c287
Guilherme Wilker <wilkerguilherme0@gmail.com>
2023-04-04 19:30:44 -0300

    feat: criando a rota de links

--- src/views/LinksView.vue
+++ src/views/LinksView.vue
@@ -1,13 +1,16 @@
 <template>
-  <div>
-    <h3>LINK HOME</h3>
-    <div>
-      <input type="text" v-model="inputValue" placeholder="https://.." />
-      <button @click="createLink">Add</button>
-    </div>
+  <main>
+    <header>
+      <h3>Sites de Referências para Design</h3>
+
+      <div class="linkModal">
+        <input type="text" v-model="inputValue" placeholder="https://.." />
+        <button @click="createLink">Add</button>
+      </div>
+    </header>
 
     {{ linksCollection }}
-  </div>
+  </main>
 </template>
 
 <script setup>
@@ -23,4 +26,32 @@ function createLink() {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+header {
+  margin: 150px 0;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+main {
+  margin: 0 auto;
+  max-width: 960px;
+}
+
+.linkModal {
+  width: 353px;
+
+  padding: 10px;
+
+  background: #ffffff;
+  box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.25);
+  border-radius: 3px;
+}
+
+.linkModal input {
+  padding: 9px 10px;
+  width: 290px;
+}
+</style>
